efc245545694368ce83a80f2f7f3adb5cfe7432a
8rs1 <arssani.info@gmail.com>
2023-11-26 16:19:43 +0330

    newest

--- home/tailwind.config.js
+++ home/tailwind.config.js
@@ -1,11 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html"],
+  content: ["*.html","js/*.js"],
   theme: {
-    extend: {},
     container: {
-      center: true
-    }
+      center: true,
+    },
+    extend: {
+      animation: {
+        forbtn: 'widthhl 800ms ease 1  normal'
+      },
+      keyframes: {
+        widthhl: {
+          '0%': {width: '0'},
+          '25%': {width: '75%'},
+          '50%': {width: '100%'},
+          '75%': {width: '85%'},
+          '100%': {width: '100%'},
+        }
+      }
+    },
   },
   plugins: [],
 }
